a4a7670317e9e93ab030a90c716711a2278b26fd
Lucas Paim <lucaspaimrj21@gmail.com>
2025-03-18 23:07:31 -0300

    PUT product

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -41,15 +41,27 @@ public class ProductService {
 	public ProductDTO insert(ProductDTO dto) {
 		
 		Product entity = new Product();
+		copyDTOtoEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+	
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+		
+		Product entity = repository.getReferenceById(id);
+		copyDTOtoEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+
+	private void copyDTOtoEntity(ProductDTO dto, Product entity) {
 		entity.setName(dto.getName());
 		entity.setDescription(dto.getDescription());
 		entity.setPrice(dto.getPrice());
 		entity.setImgUrl(dto.getImgUrl());
 		
-		entity = repository.save(entity);
-		
-		return new ProductDTO(entity);
 	}
-		
+	
 
 }
